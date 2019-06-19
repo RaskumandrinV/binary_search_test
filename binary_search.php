4c1682e4f53066c6324aa3446f1c4e55b61bc512
vadim <vadim@raskumandrin.ru>
2019-06-19 13:03:32 +0300

    second_version

--- binary_search.php
+++ binary_search.php
@@ -1,38 +1,29 @@
 <?
-function binarySearch ($file,$key){
-    $handle = fopen($file, "r");
-    $end_of_block = '';
-    $arr = '';
-    while(!feof($handle)){
-        $inner_block = fgets($handle,4000);
-        mb_convert_encoding($inner_block,'cp1251');
-        $inner_block = $end_of_block.$inner_block;
-        $end_of_block = preg_replace('/ключ.*x0A/i','',$inner_block);
-        $length_end_of_block = -1 * strlen($end_of_block);
-        $inner_block = substr($inner_block, 0, $length_end_of_block);
-        $exploded = explode('\x0A',$inner_block);
-        array_pop($exploded);
-        foreach($exploded as $k=>$v){
-            $arr[] = explode('\t',$v);
-        }
-        $begin = 0;
-        $end = count($arr)-1;
-        while($begin<=$end) {
-            $middle_value = floor(($begin+$end)/2);
-            $string_compare=strnatcmp($arr[$middle_value][0],$key);
-            if($string_compare>0){
-                $end = $middle_value-1;
-            } elseif($string_compare<0) {
-                $begin = $middle_value+1;
-            } else {
-                return $arr[$middle_value][1];
-            }
+function binarySearch($file_name,$key) {
+    $file = new SplFileObject($file_name);
+
+    $beg = 0;
+    $file->seek($file->getSize());
+    $end = $file->key();
+
+    while($beg<=$end) {
+        $middle_value = floor(($beg+$end)/2);
+        $file->seek($middle_value);
+        $delimeter = "\t";
+        $exploded_string = explode($delimeter,$file->current());
+        $string_compare=strnatcmp($exploded_string[0],$key);
+        if($string_compare>0){
+            $end = $middle_value-1;
+        } elseif($string_compare<0) {
+            $beg = $middle_value+1;
+        } else {
+            return $exploded_string[1];
         }
     }
-    return 'undefined';
+    return 'undef';
 }
 
 $file = 'file.txt';
-$key = 'ключ343';
+$key = 'key343';
 echo binarySearch($file,$key)."<br />";
 ?>
